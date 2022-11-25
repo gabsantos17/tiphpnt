97b63910b27258af91a2c7e80670152f52fe1b16
Gabriel Santos Bezerra <gabrielsantosbezerra081@gmail.com>
2022-11-25 15:33:17 -0300

    inicial de preparação de ambiente

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Chuleta Quente Churrascaria</title>
+</head>
+<body>
+    <h3 class="text-danger bg-info">Index</h3>
+    <h3 class="text-danger bg-success">Index</h3>
+    <style>
+        .h3{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</body>
+</html>
